7b1b8188143bbada93c04caaae9cdd518c742dfa
Keval Dholakiya <kevald47@gmail.com>
2020-02-19 10:46:02 -0800

    Create plateDetail.py

---
+++ src/detect/plateDetail.py
@@ -0,0 +1,124 @@
+import pytesseract
+import argparse
+import sys
+import re
+import os
+import requests
+import cv2
+import json 
+import numpy as np
+from time import sleep
+from PIL import Image, ImageEnhance
+from bs4 import BeautifulSoup, SoupStrainer
+from urllib.request import urlretrieve
+from io import BytesIO
+
+s = requests.Session()
+
+pytesseract.pytesseract.tesseract_cmd = "J:/Program Files/Tesseract-OCR/tesseract.exe"
+
+app_url = 'https://vahan.nic.in/nrservices/faces/user/searchstatus.xhtml'
+captcha_image_url = 'https://vahan.nic.in/nrservices/cap_img.jsp'
+number = sys.argv[1]
+
+#MARK: Get Request to get webpage elements like textFields, image, etc
+r = s.get(url=app_url)
+cookies = r.cookies
+soup = BeautifulSoup(r.text, 'html.parser')
+
+
+#MARK: ViewState contains token which needs to be passed in POST Request
+# ViewState is a hidden element. Open debugger to inspect element 
+viewstate = soup.select('input[name="javax.faces.ViewState"]')[0]['value']
+
+
+#MARK: Get Request to get Captcha Image from URL
+## Captcha Image Changes each time the URL is fired
+iresponse = s.get(captcha_image_url)
+img = Image.open(BytesIO(iresponse.content))
+img.save("downloadedpng.png")
+
+def resolve(img):
+	enhancedImage = enhance()
+	return pytesseract.image_to_string(enhancedImage)
+
+def enhance():
+	img = cv2.imread('downloadedpng.png', 0)
+	kernel = np.ones((2,2), np.uint8)
+	img_erosion = cv2.erode(img, kernel, iterations=1)
+	img_dilation = cv2.dilate(img, kernel, iterations=1)
+	erosion_again = cv2.erode(img_dilation, kernel, iterations=1)
+	final = cv2.GaussianBlur(erosion_again, (1, 1), 0)
+	return final
+
+
+print('Resolving Captcha')
+captcha_text = resolve(img)
+extracted_text = str.upper(captcha_text.replace(" ", "").replace("\n", ""))
+print("OCR Result => ", extracted_text)
+# print(extracted_text)
+
+# MARK: Identifying Submit Button which will be responsible to make POST Request
+button = soup.find("button",{"type": "submit"})
+
+
+encodedViewState = viewstate.replace("/", "%2F").replace("+", "%2B").replace("=", "%3D")
+
+# MARK: Data, which needs to be passed in POST Request | Verify this manually in debugger
+data = {
+	'javax.faces.partial.ajax':'true',
+	'javax.faces.source': button['id'],
+	'javax.faces.partial.execute':'@all',
+	'javax.faces.partial.render': 'rcDetailsPanel resultPanel userMessages capatcha txt_ALPHA_NUMERIC',
+	button['id']:button['id'],
+	'masterLayout':'masterLayout',
+	'regn_no1_exact': number,
+	'txt_ALPHA_NUMERIC': extracted_text,
+	'javax.faces.ViewState': viewstate,
+	'j_idt32':''
+}
+# MARK: Data in Query format.. But not in use for now
+query = "javax.faces.partial.ajax=true&javax.faces.source=%s&javax.faces.partial.execute=%s&javax.faces.partial.render=rcDetailsPanel+resultPanel+userMessages+capatcha+txt_ALPHA_NUMERIC&j_idt42=j_idt42&masterLayout=masterLayout&j_idt32=&regn_no1_exact=%s&txt_ALPHA_NUMERIC=%s&javax.faces.ViewState=%s"%(button['id'], '%40all', number, extracted_text, encodedViewState)
+
+
+# MARK: Request Headers which may or may not needed to be passed in POST Request
+# Verify in debugger
+headers = {
+		"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:74.0) Gecko/20100101 Firefox/74.0",
+        "Accept": "application/xml, text/xml, */*; q=0.01",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+        "Faces-Request": "partial/ajax",
+        "X-Requested-With": "XMLHttpRequest"
+}
+# print(headers)
+
+# print("\nCookie JSESSIONID => ", cookies['JSESSIONID'])
+# print("\nData => \n")
+# print(data)
+
+# MARK: Added delay
+sleep(2.0)
+
+
+
+#MARK: Send POST Request 
+postResponse = s.post(url=app_url, data=data, headers=headers, cookies=cookies)
+print("\nPOST Request -> Response =>\n")
+
+rsoup = BeautifulSoup(postResponse.text, 'html.parser')
+print("Mark: postResponse soup => ")
+print(rsoup.prettify())
+
+#MARK: Following code finds tr which means <table> element from html response
+# the required response is appended in <table> only. Verify it in debugger
+table = SoupStrainer('tr')
+tsoup = BeautifulSoup(rsoup.get_text(), 'html.parser', parse_only=table)
+
+print("Table Soup => ")
+print(tsoup.prettify())
+#MARK: Result Table not appending to the response data
+#Fix Needed
+
+
+
