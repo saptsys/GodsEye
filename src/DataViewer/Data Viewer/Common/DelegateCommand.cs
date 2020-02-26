380c7c53d533a985c41007a06011fbfb2bd016ec
Vaitul Bhayani <39405950+vaitul@users.noreply.github.com>
2020-02-27 00:41:53 +0530

    +++

---
+++ src/DataViewer/Data Viewer/Common/DelegateCommand.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Data_Viewer.Common
+{
+//#pragma warning disable CS0067
+    public class DelegateCommand : ICommand
+    {
+        private Action<object> Action;
+        private Predicate<object> canExec;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public DelegateCommand(Action<object> action,Predicate<object> canexec = null)
+        {
+            if (action != null)
+                this.Action = action;
+            if (canexec != null)
+                this.canExec = canexec;
+        }
+        
+        public bool CanExecute(object parameter)
+        {
+            if (this.canExec != null)
+            {
+                return canExec.Invoke(parameter);
+            }
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (this.Action != null)
+                Action.Invoke(parameter);
+        }
+    }
+}
---
+++ src/DataViewer/Data Viewer/ViewModels/BaseViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Viewer.ViewModels
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public bool SetValue<T>(ref T property,T value,[CallerMemberName] string propertyName = null)
+        {
+            if (Equals(value, property))
+                return false;
+
+            property = value;
+            OnPropertyChanged(propertyName);
+
+            return true;
+        }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
